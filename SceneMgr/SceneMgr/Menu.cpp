b7bafa38e2f263edfe629986c50be4953b70a626
nooozu <riku627nr@gmail.com>
2016-12-17 02:23:39 +0900

    Menuクラスの追加

---
+++ SceneMgr/SceneMgr/Menu.cpp
@@ -0,0 +1,21 @@
+#include "Menu.h"
+
+Menu::Menu(ISceneChanger* changer) : BaseScene(changer)
+{
+}
+
+void Menu::Initialize()
+{
+}
+
+void Menu::Finalize()
+{
+}
+
+void Menu::Update()
+{
+}
+
+void Menu::Draw()
+{
+}
