059929f94098f12300bc617fd59098a26f361140
nooozu <riku627nr@gmail.com>
2016-12-17 02:04:55 +0900

    シーンを変更するためのインターフェース

---
+++ SceneMgr/SceneMgr/ISceneChanger.h
@@ -0,0 +1,19 @@
+#pragma once
+
+typedef enum {
+	eScene_Title,    //タイトル画面
+	eScene_Menu,    //メニュー画面
+	eScene_Game,    //ゲーム画面
+	eScene_Config,  //設定画面
+
+	eScene_None,    //無し
+} eScene;
+
+//シーンを変更するためのインターフェイスクラス
+class ISceneChanger {
+public:
+	virtual ~ISceneChanger() = 0;
+
+	//指定シーンに変更する
+	virtual void ChangeScene(eScene NextScene) = 0;
+};
